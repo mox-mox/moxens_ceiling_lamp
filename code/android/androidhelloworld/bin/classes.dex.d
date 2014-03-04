b9e5f7476e68e08a62f97551f390a903e91804e6
mox <de_mox@zoho.com>
2014-03-04 03:03:50 +0100

    seems to work more or less

--- code/android/androidhelloworld/bin/classes.dex.d
+++ code/android/androidhelloworld/bin/classes.dex.d
@@ -1,10 +1,10 @@
 /dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes.dex : \
-/dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes/mox/lamp/myfirstapp/DisplayMessageActivity.class \
-/dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes/mox/lamp/myfirstapp/R$id.class \
 /dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes/mox/lamp/myfirstapp/R.class \
 /dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes/mox/lamp/myfirstapp/R$string.class \
 /dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes/mox/lamp/myfirstapp/R$layout.class \
+/dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes/mox/lamp/myfirstapp/R$id.class \
 /dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes/mox/lamp/myfirstapp/R$attr.class \
 /dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes/mox/lamp/myfirstapp/BuildConfig.class \
 /dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes/mox/lamp/myfirstapp/MainActivity.class \
+/dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/classes/mox/lamp/myfirstapp/DisplayMessageActivity.class \
 /dev/shm/asd-mox/home/mox/.eagle/projects/Zimmerlampe/code/android/androidhelloworld/bin/dexedLibs/android-support-v4-760a5d9eb72bd25bd6d1dfaa4c7aaa9e.jar \
